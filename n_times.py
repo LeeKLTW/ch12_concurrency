d9570d3b211319480e0f50199998a5c652915a04
LeeKLTW <LeeKLTW@gmail.com>
2019-02-15 18:59:09 +0800

    n_times

--- n_times.py
+++ n_times.py
@@ -27,5 +27,25 @@ class PeriodicTimer:
             while last_flag == self._flag:
                 self._cv.wait()
 
+
 ptimer = PeriodicTimer(5)
 ptimer.start()
+
+
+def countdown(nticks):
+    while nticks > 0:
+        ptimer.wait_for_tick()
+        print('T - minus', nticks)
+        nticks -= 1
+
+
+def countup(last):
+    n = 0
+    while n < last:
+        ptimer.wait_for_tick()
+        print('Counting', n)
+        n += 1
+
+
+threading.Thread(target=countdown, args=(10,)).start()
+threading.Thread(target=countup, args=(5,)).start()
