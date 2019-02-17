c30469e5f275042adc72e553708be46f48fa5575
LeeKLTW <LeeKLTW@gmail.com>
2019-02-17 21:35:19 +0800

    semaphore

--- sema.py
+++ sema.py
@@ -1,1 +1,21 @@
 # encoding: utf-8
+import threading
+
+
+def worker(n, sema):
+    sema.acquire()
+    print('working', n)
+
+sema = threading.Semaphore(0)
+nworkers = 10
+
+for n in range(nworkers):
+    t = threading.Thread(target=worker, args=(n ,sema,))
+    t.start()
+
+# nothing will happend now
+
+# semaphore release:
+# >>
+for i in range(nworkers):
+    sema.release()
